7e2e95e084393c48fda7f5a5edc98465abd39d86
Mert Büyük <63505261+mertbuyuk@users.noreply.github.com>
2021-06-27 13:20:04 +0300

    search

--- src/main/kotlin/searching/BinarySearch.kt
+++ src/main/kotlin/searching/BinarySearch.kt
@@ -1,4 +1,21 @@
 package searching
 
-class BinarySearch {
+class BinarySearch<T> {
+
+    fun binarySearch(arr: Array<T>, item: T): Int {
+        var low = 0
+        var high = arr.size-1
+
+        while (low <= high){
+
+            val middle = (low + high)/2
+            when{
+                item < arr[middle] -> high = middle -1
+                item > arr[middle] -> low = middle + 1
+                else -> return middle
+            } }
+
+        return -1
+    }
+
 }
